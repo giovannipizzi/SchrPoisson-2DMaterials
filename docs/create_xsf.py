156917a347530422fba2dd6e1490c1860e204c69
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2017-05-02 15:29:34 +0200

    Removed unneeded file, renamed file with typo

--- docs/create_xsf.py
+++ docs/create_xsf.py
@@ -10,11 +10,16 @@ x_pos = np.append(x_positions_1, x_positions_2)
 x_pos = np.append(x_pos,x_positions_3)
 
 
-cell = ase.Atoms('C'*x_pos.size,pbc=True, cell = ((x_pos[-1],0.,0.),
-						    (0.,3.0,0.),
-						    (0.,0.,15.0)))
+cell = ase.Atoms('C'*x_pos.size,
+                 pbc=True, 
+                 cell = ( (x_pos[-1],0.,0.),
+                          (0.,3.0,0.),
+                          (0.,0.,15.0)))
 
-cell.set_atomic_numbers([6,6,6,16,16,16,16,16,16,6,6,6])
+cell.set_atomic_numbers([6, 6, 6,
+                         16, 16, 16, 16, 16, 16, 
+                         6, 6, 6])
+
 pos = []
 for i in range(x_pos.size):
     pos.append([x_pos[i],0.0,7.5])
